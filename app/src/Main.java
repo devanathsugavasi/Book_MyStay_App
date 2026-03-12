502c5bda7b424cb9351000bd00a31e015905de95
devasugavasi <devanathsugavasi01@gmail.com>
2026-03-12 09:05:23 +0530

    Devasugavasi Added feature/UC1-WelcomePage

---
+++ app/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * BookMyStayApp
+ * Entry point for the Hotel Booking Application
+ *
+ * @author Devasugavasi
+ * @version 1.0
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("=================================");
+
+    }
+}
